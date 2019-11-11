7650853d991010a2e5c0a793ffe561fd997415c5
VictorBudal <55115446+VictorBudal@users.noreply.github.com>
2019-11-11 09:24:20 -0300

    Update ReturnKing.java

--- src/br/usp/ime/maratona/warmup/solutions/ReturnKing.java
+++ src/br/usp/ime/maratona/warmup/solutions/ReturnKing.java
@@ -2,15 +2,66 @@ package br.usp.ime.maratona.warmup.solutions;
 
 import br.usp.ime.maratona.warmup.Desafio;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 
 public class ReturnKing implements Desafio<String> {
     
    
     @Override
     public String executar(File input) {
-        
-        // Adicione sua solução aqui!
-  
+        try{
+            Scanner scan = new Scanner(input);
+
+            DecimalFormat formato = new DecimalFormat("0,00");
+
+            String entrada = scan.next();
+
+            int tamanho = entrada.length();
+
+            double soma= 0;
+
+            int multiplicador=0;
+
+            int tabela[]= new int [tamanho];
+
+            //separa numeros
+            for(int i = 0;i<tamanho;i++){
+                tabela[i]=entrada.charAt(i) - '0';
+            }
+            
+            //Quantos numeros existem
+            for(int i = 0 ; i<tamanho;i++){
+                if(tabela[i]==0){
+                    multiplicador-=1;
+                }
+                multiplicador+=1;
+            }
+            
+            //Soma dos numeros
+            for(int i =0 ; i<tamanho;i++){
+                if(tabela[i]==1){
+                        soma+=1;
+                    }
+                if(tabela[i]==0){
+                    soma+=9;
+                }
+                if(tabela[i]>1){
+                    soma+=tabela[i];
+                }
+            }
+            
+            soma/=multiplicador;
+            soma*=100;
+            String resposta=formato.format(soma);
+            return resposta;
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            
+        }
         return null;
     }
 }
