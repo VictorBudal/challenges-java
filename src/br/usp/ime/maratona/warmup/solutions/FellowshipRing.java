12ff146bfc60f96d02635fb85a727c924147d6a8
VictorBudal <55115446+VictorBudal@users.noreply.github.com>
2019-11-11 09:22:50 -0300

    Update FellowshipRing.java

--- src/br/usp/ime/maratona/warmup/solutions/FellowshipRing.java
+++ src/br/usp/ime/maratona/warmup/solutions/FellowshipRing.java
@@ -2,6 +2,8 @@ package br.usp.ime.maratona.warmup.solutions;
 
 import br.usp.ime.maratona.warmup.Desafio;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 
 public class FellowshipRing implements Desafio<Integer> {
@@ -9,7 +11,30 @@ public class FellowshipRing implements Desafio<Integer> {
     @Override
     public Integer executar(File input) {
         
-       // Adicione sua solução aqui! 
+       try {
+            Scanner scan = new Scanner(input);
+           
+            int cintura = scan.nextInt();
+            int pescoco = scan.nextInt();
+            int socos = scan.nextInt();
+            int alturaSocos=0;
+            int socosValidos=0;
+
+            for(int i  = 0 ; i<socos;i++){
+                alturaSocos=scan.nextInt();
+                if(alturaSocos>=cintura && alturaSocos<=pescoco){
+                    socosValidos++;
+                }
+
+            }
+
+            return socosValidos;
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            
+            }
 
         return null;
     }    
